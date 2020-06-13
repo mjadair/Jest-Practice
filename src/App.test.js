64ff692bf0f1583610027e83662b7c4259f4fc28
Michael Adair <mjadair91@gmail.com>
2020-06-13 13:24:04 +0100

    added enzyme and config

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+
+test('renders app without crashing', () => {
+  //checks our app component, and the test fails if it is unable to render
+  const wrapper = shallow(<App />)
+  //using debug returns our DOM as a string.
+  console.log(wrapper.debug())
+
+
+  expect(wrapper).toBeTruthy()
+
+
+})
