a153904467b9b15e9035faa68a11147fe238fde6
Michael Adair <mjadair91@gmail.com>
2020-06-26 15:43:11 +0100

    fleshed out tests

--- jotto-game/src/GuessedWords.Test.js
+++ jotto-game/src/GuessedWords.Test.js
@@ -20,9 +20,23 @@ test('does not throw warning with expected props', () => {
 
 
 describe('if there are no words guessed', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: []})
+  })
+  test('renders without erropr', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1)
+  })
+
+  test('renders instructions to guess a word', () =>{
+    const instructions = findByTestAttr(wrapper, 'guess-instructions')
+    expect(instructions.text().length).not.toBe(0)
+  })
 
 })
 
 describe('if there are words guessed', () => {
-  
+
 })
